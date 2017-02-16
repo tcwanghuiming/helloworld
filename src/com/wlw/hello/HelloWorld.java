2a8ee665b59d863f90a06af54ee4c0babdcdd8d4
tcwanghuiming <tcwanghuiming2@163.com>
2017-02-16 14:29:04 +0800

    Initial commit

---
+++ src/com/wlw/hello/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.wlw.hello;
+
+
+import java.util.Random;
+
+/**
+ * Created by whm on 2017/2/14.
+ */
+public class HelloWorld {
+    private String s="1";
+    Random r = new Random();
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
