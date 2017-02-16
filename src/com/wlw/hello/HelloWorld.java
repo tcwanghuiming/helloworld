aa1646464bd89c0ad38dfd1934bddae7f8d89c27
tcwanghuiming <tcwanghuiming2@163.com>
2017-02-16 14:52:18 +0800

    add string ss

--- src/com/wlw/hello/HelloWorld.java
+++ src/com/wlw/hello/HelloWorld.java
@@ -18,6 +18,7 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        String ss = "hello world!";
+        System.out.println(ss);
     }
 }
