a32c81d8e34bb6a7531ec67e4d9130638a256613
tcwanghuiming <tcwanghuiming2@163.com>
2017-06-09 00:15:19 +0800

    添加注释4

--- src/com/wlw/hello/HelloWorld.java
+++ src/com/wlw/hello/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
     public void setS(String s) {
         this.s = s;
     }
-//配置1231231
+//配置123123134
     public static void main(String[] args) {
         String ss = "hello world!";
         System.out.println(ss);
